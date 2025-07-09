67fe18326ae2c60fdb23018f81de18f596e58336
Reema Raghava <reema14a@gmail.com>
2025-07-09 21:13:10 +0530

    Added form to add item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,32 +1,93 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
 
-class GroceryList extends StatelessWidget {
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(builder: (ctx) => const NewItem()),
+    );
+
+    if (newItem == null) {
+      return;
+    } else {
+      setState(() {
+        _groceryItems.add(newItem);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Your Groceries')),
-      body: Padding(
+    Widget mainContent = const Center(
+      child: Text('No items added yet.'),
+    );
+    if (_groceryItems.isNotEmpty) {
+      mainContent = Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
           child: ListView.builder(
-            itemCount: groceryItems.length,
+            itemCount: _groceryItems.length,
             itemBuilder: (ctx, index) {
-              return ListTile(
-                title: Text(groceryItems[index].name),
-                leading: Container(
-                  width: 24,
-                  height: 24,
-                  color: groceryItems[index].category.color,
+              return Dismissible(
+                key: ValueKey(_groceryItems[index]),
+                onDismissed: (direction) {
+                  setState(() {
+                    _groceryItems.removeAt(index);
+                    ScaffoldMessenger.of(context).clearSnackBars();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Item deleted.'),
+                      ),
+                    );
+                  });
+                },
+                background: Container(
+                  color: Theme.of(
+                    context,
+                  ).colorScheme.error,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
                 ),
-                trailing: Text(groceryItems[index].quantity.toString()),
+
+                child: ListTile(
+                  title: Text(_groceryItems[index].name),
+                  leading: Container(
+                    width: 24,
+                    height: 24,
+                    color: _groceryItems[index].category.color,
+                  ),
+                  trailing: Text(_groceryItems[index].quantity.toString()),
+                ),
               );
             },
           ),
         ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
+      body: mainContent,
     );
   }
 }
